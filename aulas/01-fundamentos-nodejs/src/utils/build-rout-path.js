873a7d5a78558835c4d5ccfe0e4b9d561b84af3f
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-08-29 23:41:32 -0300

    feat: get params with regex

---
+++ aulas/01-fundamentos-nodejs/src/utils/build-rout-path.js
@@ -0,0 +1,11 @@
+export function buildRoutePath(path) {
+  const routeParamaterRegex = /:([a-zA-Z]+)/g;
+  const pathWithParams = path.replaceAll(
+    routeParamaterRegex,
+    "(?<$1>[a-z0-9-_]+)"
+  );
+
+  const pathRegex = new RegExp(`^${pathWithParams}`);
+
+  return pathRegex;
+}
