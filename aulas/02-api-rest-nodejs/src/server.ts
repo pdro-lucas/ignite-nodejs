993ff68328b4d71a9e094df7d4bc887546a0891b
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-02 18:14:04 -0300

    feat: create migrations and validate env with zod

--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -1,16 +1,18 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import { env } from './env';
 
 const app = fastify();
 
 app.get('/', async (request, reply) => {
-  const tables = await knex('sqlite_schema').select('*');
-  return tables;
+  const transactions = await knex('transactions').select('*');
+
+  return transactions;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server running on http://localhost:3333');
