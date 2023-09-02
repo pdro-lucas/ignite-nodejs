896be52e21ad7291be6cee1e9cfecd4124bbcda1
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-02 15:40:32 -0300

    feat: configure knexjs with ts

---
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify';
+import { knex } from './database';
+
+const app = fastify();
+
+app.get('/', async (request, reply) => {
+  const tables = await knex('sqlite_schema').select('*');
+  return tables;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333');
+  });
