72cb0e148569dbb283da1ffaae846d8453addb19
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-02 19:09:57 -0300

    feat: add route to create transactions

--- aulas/02-api-rest-nodejs/src/server.ts
+++ aulas/02-api-rest-nodejs/src/server.ts
@@ -1,14 +1,10 @@
 import fastify from 'fastify';
-import { knex } from './database';
 import { env } from './env';
+import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.get('/', async (request, reply) => {
-  const transactions = await knex('transactions').select('*');
-
-  return transactions;
-});
+app.register(transactionsRoutes, { prefix: 'transactions' });
 
 app
   .listen({
