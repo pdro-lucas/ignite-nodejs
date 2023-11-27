a9e7f630e26748edb308352d52884981207ab316
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-27 16:49:04 -0300

    test if user can authenticate

---
+++ aulas/api-solid/src/use-cases/autheticate.spec.ts
@@ -0,0 +1,24 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { AuthenticateUseCase } from './authenticate'
+
+describe('Authenticate Use Case', () => {
+  it('should be able to authenticate', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'john2@example.com',
+      password_hash: await hash('12345678', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'john2@example.com',
+      password: '12345678',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+})
