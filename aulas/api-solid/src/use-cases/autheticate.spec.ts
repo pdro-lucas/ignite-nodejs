f44ef579d11c75df3240e18f0d9d77c14fe12ba6
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-27 16:52:39 -0300

    feat? Add test case for invalid credentials

--- aulas/api-solid/src/use-cases/autheticate.spec.ts
+++ aulas/api-solid/src/use-cases/autheticate.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user
 import { hash } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
 import { AuthenticateUseCase } from './authenticate'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
 describe('Authenticate Use Case', () => {
   it('should be able to authenticate', async () => {
@@ -20,5 +21,17 @@ describe('Authenticate Use Case', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    expect(
+      async () =>
+        await sut.execute({
+          email: 'john2@example.com',
+          password: '12345678',
+        }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
