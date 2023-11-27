5d05817fe2c6352ddde7edf3966c07a2aa857ab5
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-27 01:17:45 -0300

    feat: Add register use case tests

---
+++ aulas/api-solid/src/use-cases/register.spec.ts
@@ -0,0 +1,61 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { RegisterUseCase } from './register'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '12345678',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '12345678',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@gmail.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '12345678',
+    })
+
+    await expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+
+  it('should not be able register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@gmail.com'
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '12345678',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+})
