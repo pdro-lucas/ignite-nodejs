5eeadcc77778a3c33b98ed7fdb9e9e8d79fc373f
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-27 23:38:21 -0300

    feat: Add authentication route

--- aulas/api-solid/src/http/routes.ts
+++ aulas/api-solid/src/http/routes.ts
@@ -1,6 +1,8 @@
 import { register } from '@/http/controllers/register'
 import type { FastifyInstance } from 'fastify'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
