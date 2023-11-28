f9c0e2a21f5d8db8ee7278c5a805606aca2c005a
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-27 23:38:13 -0300

    feat: Add authentication controller

---
+++ aulas/api-solid/src/http/controllers/authenticate.ts
@@ -0,0 +1,35 @@
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase()
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      reply.status(400).send({
+        message: error.message,
+      })
+    }
+
+    throw error
+  }
+
+  reply.status(200).send()
+}
