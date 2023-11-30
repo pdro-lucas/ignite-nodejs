af6af7864eb6f636109bb1151cc5b593421fe61f
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-30 15:19:01 -0300

    refactor:  include date range

--- aulas/api-solid/src/repositories/in-memory/in-memory-check-in-repository.ts
+++ aulas/api-solid/src/repositories/in-memory/in-memory-check-in-repository.ts
@@ -1,12 +1,21 @@
 import { CheckIn, Prisma } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { CheckInsRepository } from '../check-ins-repository'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
-  async findByUserIdOnDate(userId: string, _date: Date) {
-    const checkInOnSameDate = this.items.find(
-      (checkIn) => checkIn.user_id === userId,
-    )
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
 
     if (!checkInOnSameDate) {
       return null
