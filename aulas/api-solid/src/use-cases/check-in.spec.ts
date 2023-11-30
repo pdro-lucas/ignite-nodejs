e1d0105ee100ef26a911b7e069f697f98741ccbb
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-30 14:45:24 -0300

    test: add unit test to prevent double check-in on the same day

--- aulas/api-solid/src/use-cases/check-in.spec.ts
+++ aulas/api-solid/src/use-cases/check-in.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-in-repository'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from './check-in'
 
 let checkInsRepository: InMemoryCheckInsRepository
@@ -9,13 +9,37 @@ describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
   })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
   it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
     })
 
+    console.log(checkIn.created_at)
+
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2023, 10, 30, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
