5bd93dc7da52214ae8bf0c91a2f70063f0cc1cb6
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-30 15:16:51 -0300

    test: add test to allow check-in on different days for the same user

--- aulas/api-solid/src/use-cases/check-in.spec.ts
+++ aulas/api-solid/src/use-cases/check-in.spec.ts
@@ -23,10 +23,9 @@ describe('Check-in Use Case', () => {
       userId: 'user-01',
     })
 
-    console.log(checkIn.created_at)
-
     expect(checkIn.id).toEqual(expect.any(String))
   })
+
   it('should not be able to check in twice in the same day', async () => {
     vi.setSystemTime(new Date(2023, 10, 30, 8, 0, 0))
 
@@ -41,5 +40,23 @@ describe('Check-in Use Case', () => {
         userId: 'user-01',
       }),
     ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    vi.setSystemTime(new Date(2023, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
   })
 })
