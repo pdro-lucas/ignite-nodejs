2fdca80dd8fb8390fa6905fed428b18ad3972e26
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-05 16:10:07 -0300

    feat: Add unit test for CreateGymUseCase

---
+++ aulas/api-solid/src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'Javascript Academy',
+      description: null,
+      phone: null,
+      latitude: -20.3932868,
+      longitude: -43.3546241,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
