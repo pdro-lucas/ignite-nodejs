c6ade67fa607b4de2320b5987a49d9960ac650c9
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-17 20:40:45 -0300

    feat: Add countByUserId method to InMemoryCheckInsRepository

--- aulas/api-solid/src/repositories/in-memory/in-memory-check-in-repository.ts
+++ aulas/api-solid/src/repositories/in-memory/in-memory-check-in-repository.ts
@@ -44,6 +44,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkIns
   }
 
+  async countByUserId(userId: string) {
+    const checkIns = this.items.filter((checkIn) => checkIn.user_id === userId)
+
+    return checkIns.length
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
