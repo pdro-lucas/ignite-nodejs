fe9e141632833ca3ee15f6c612aaef78157ca4aa
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-18 11:13:11 -0300

    feat: Add JWT verification middleware to authenticated route

--- aulas/api-solid/src/http/routes.ts
+++ aulas/api-solid/src/http/routes.ts
@@ -3,11 +3,18 @@ import type { FastifyInstance } from 'fastify'
 
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   /** AUTHENTICATED */
-  app.get('/me', profile)
+  app.get(
+    '/me',
+    {
+      onRequest: [verifyJWT],
+    },
+    profile,
+  )
 }
