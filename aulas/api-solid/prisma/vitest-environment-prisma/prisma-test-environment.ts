4e35c34f4a29546e71e118e559e0f35b1eeed9d9
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-19 16:48:50 -0300

    feat: Add generateDatabaseURL function and use it to generate the database URL

--- aulas/api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ aulas/api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,10 +1,27 @@
+import 'dotenv/config'
+
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('please provide DATABASE_URL env variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'web',
   async setup() {
-    console.log('setup')
+    const schema = randomUUID()
+
+    console.log(generateDatabaseURL(schema))
 
     return {
       teardown() {
