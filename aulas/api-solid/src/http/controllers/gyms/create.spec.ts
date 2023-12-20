0329fca130423649d46e9fe5db083903b1f61aa8
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-20 15:29:09 -0300

    feat: Add e2e test for creating a gym

---
+++ aulas/api-solid/src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(() => {
+    app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gym',
+        description: 'The best gym for Javascript developers',
+        phone: '123456789',
+        latitude: -20.3932868,
+        longitude: -43.3546241,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
