cf6681215d6d7661e5557b840a8fdaf9b2c6e14a
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-19 21:35:51 -0300

    feat: Add PrismaClient import and update setup and teardown functions

--- aulas/api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ aulas/api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,7 +1,11 @@
+import { PrismaClient } from '@prisma/client'
 import 'dotenv/config'
+import { execSync } from 'node:child_process'
 
 import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
 
 function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
@@ -20,12 +24,19 @@ export default <Environment>{
   transformMode: 'web',
   async setup() {
     const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
 
-    console.log(generateDatabaseURL(schema))
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() {
-        console.log('teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
