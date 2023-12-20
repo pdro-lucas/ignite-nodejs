b4451aa58a8d73efeaab3162591fb5309fcfb195
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-20 11:44:37 -0300

    feat: Add check-ins history, metrics, and validate endpoints

--- aulas/api-solid/src/http/controllers/check-ins/routes.ts
+++ aulas/api-solid/src/http/controllers/check-ins/routes.ts
@@ -2,9 +2,17 @@ import type { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { create } from './create'
+import { history } from './history'
+import { metrics } from './metrics'
+import { validate } from './validate'
 
 export async function checkInsRoutes(app: FastifyInstance): Promise<void> {
   app.addHook('onRequest', verifyJWT)
 
+  app.get('/check-inshistory', history)
+  app.get('/check-ins/metrics', metrics)
+
   app.post('/gyms/:gymId/check-ins', create)
+
+  app.patch('/check-ins/:checkInId/validate', validate)
 }
