abd9ee691e81d9016bf8dba5f340837bc86f3f6d
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-20 21:33:57 -0300

    feat: Add refresh token route

--- aulas/api-solid/src/http/controllers/users/routes.ts
+++ aulas/api-solid/src/http/controllers/users/routes.ts
@@ -4,10 +4,13 @@ import type { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance): Promise<void> {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** AUTHENTICATED */
   app.get(
