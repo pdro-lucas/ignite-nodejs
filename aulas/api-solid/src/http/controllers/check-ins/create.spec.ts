60d213b394ddfe477b090992ff46f4e8a9c8f4d0
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-20 21:01:45 -0300

    feat: Add e2e test for creating check-ins

---
+++ aulas/api-solid/src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,39 @@
+import { app } from '@/app'
+import { prisma } from '@/lib/prisma'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(() => {
+    app.close()
+  })
+
+  it('should be able to create check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Javascript Gym',
+        description: 'The best gym for Javascript developers',
+        phone: '123456789',
+        latitude: -20.3932868,
+        longitude: -43.3546241,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -20.3932868,
+        longitude: -43.3546241,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
