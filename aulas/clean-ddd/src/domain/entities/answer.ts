9bae4841de7086902230fbbec645e7e3d057823c
pdro-lucas <pdrolucas.dev@gmail.com>
2024-06-02 18:57:26 -0300

    feat: adiciona os getters e setters das entidades

--- aulas/clean-ddd/src/domain/entities/answer.ts
+++ aulas/clean-ddd/src/domain/entities/answer.ts
@@ -10,8 +10,37 @@ interface AnswerProps {
   updatedAt?: Date;
 }
 export class Answer extends Entity<AnswerProps> {
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
   get content() {
     return this.props.content;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
   }
 
   static create(
