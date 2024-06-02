502c0fda5586ae52d98dfaba1d9f28ff9a927114
pdro-lucas <pdrolucas.dev@gmail.com>
2024-06-01 21:21:42 -0300

    feat: id das entidades

--- aulas/clean-ddd/src/domain/core/entities/entity.ts
+++ aulas/clean-ddd/src/domain/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from 'node:crypto'
+import { UniqueEntityID } from "./unique-entity-id";
 
 export class Entity<Props> {
-  private _id: string
-  protected props: Props
+  private _id: UniqueEntityID;
+  protected props: Props;
 
   get id() {
-    return this._id
+    return this._id;
   }
 
   public constructor(props: Props, id?: string) {
-    this.props = props
-    this._id = id ?? randomUUID()
+    this.props = props;
+    this._id = new UniqueEntityID(id);
   }
 }
