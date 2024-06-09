b4e7b41d46e26b1849b250a9b3b8993244a45bd3
pdro-lucas <pdrolucas.dev@gmail.com>
2024-06-09 16:45:40 -0300

    feat(question): add GetQuestionBySlug use case and test

---
+++ aulas/clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,33 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryQuestionsRepository } from 'test/in-memory-questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question by Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = Question.create({
+      title: 'Example Question',
+      content: 'Example Content',
+      slug: Slug.create('example-question'),
+      authorId: new UniqueEntityID(),
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.excute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual(question.title)
+  })
+})
---
+++ aulas/clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,28 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionRepository } from '../repositories/questions.repository'
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string
+}
+
+interface GetQuestionBySlugUseCaseResponse {
+  question: Question
+}
+
+export class GetQuestionBySlugUseCase {
+  constructor(private questionRepository: QuestionRepository) {}
+
+  async excute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionRepository.findBySlug(slug)
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    return {
+      question,
+    }
+  }
+}
