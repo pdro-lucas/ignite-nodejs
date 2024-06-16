3f467859ade441aa201024babbc6c1979b20ce6c
pdro-lucas <pdrolucas.dev@gmail.com>
2024-06-16 18:43:22 -0300

    feat: Implement ChooseQuestionBestAnswer use case

---
+++ aulas/clean-ddd/src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -0,0 +1,48 @@
+import type { Question } from '../../enterprise/entities/question'
+import { AnswersRepository } from '../repositories/answers.repository'
+import type { QuestionRepository } from '../repositories/questions.repository'
+
+interface ChooseQuestionBestAnswerUseCaseRequest {
+  answerId: string
+  authorId: string
+}
+
+interface ChooseQuestionBestAnswerUseCaseResponse {
+  question: Question
+}
+
+export class ChooseQuestionBestAnswerUseCase {
+  constructor(
+    private questionRepository: QuestionRepository,
+    private answersRepository: AnswersRepository,
+  ) {}
+
+  async excute({
+    authorId,
+    answerId,
+  }: ChooseQuestionBestAnswerUseCaseRequest): Promise<ChooseQuestionBestAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found')
+    }
+
+    const question = await this.questionRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Unauthorized')
+    }
+
+    question.bestAnswerId = answer.id
+
+    await this.questionRepository.save(question)
+
+    return { question }
+  }
+}
