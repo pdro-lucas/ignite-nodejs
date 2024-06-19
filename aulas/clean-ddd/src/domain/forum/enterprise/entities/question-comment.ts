db352931a48f054d0a6d2c562f0cdc03575dec41
pdro-lucas <pdrolucas.dev@gmail.com>
2024-06-18 22:43:03 -0300

    feat: Refactor comment entities and add common base class

--- aulas/clean-ddd/src/domain/forum/enterprise/entities/question-comment.ts
+++ aulas/clean-ddd/src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,38 +1,14 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, type CommentProps } from './comment'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
 
-  get content() {
-    return this.props.content
-  }
-
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+export class QuestionComment extends Comment<QuestionCommentProps> {
+  get questionId() {
+    return this.props.questionId
   }
 
   static create(
