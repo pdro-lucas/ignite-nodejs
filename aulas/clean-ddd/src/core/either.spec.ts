1d6212e7816ca7787585f42940dfc74b5a24d0c0
pdro-lucas <pdrolucas.dev@gmail.com>
2024-06-22 21:08:17 -0300

    feat: Add Either class and tests for success and error results

---
+++ aulas/clean-ddd/src/core/either.spec.ts
@@ -0,0 +1,23 @@
+import { left, right, type Either } from './either'
+
+function doSomething(shouldSuccess: boolean): Either<string, string> {
+  if (shouldSuccess) {
+    return right('success')
+  } else {
+    return left('error')
+  }
+}
+
+test('success result', () => {
+  const successResult = doSomething(true)
+
+  expect(successResult.isRight()).toBe(true)
+  expect(successResult.isLeft()).toBe(false)
+})
+
+test('error result', () => {
+  const errorResult = doSomething(false)
+
+  expect(errorResult.isLeft()).toBe(true)
+  expect(errorResult.isRight()).toBe(false)
+})
