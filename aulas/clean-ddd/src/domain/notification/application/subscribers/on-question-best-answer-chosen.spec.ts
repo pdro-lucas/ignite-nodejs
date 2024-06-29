188df8c1063a9bdd2a926a994de33c15f1b72780
pdro-lucas <pdrolucas.dev@gmail.com>
2024-06-29 18:40:15 -0300

    feat: Add unit test for OnQuestionBestAnswerChosen subscriber

---
+++ aulas/clean-ddd/src/domain/notification/application/subscribers/on-question-best-answer-chosen.spec.ts
@@ -0,0 +1,68 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answer-repository'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-questions-attachments-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { waitFor } from 'test/utils/wait-for'
+import type { MockInstance } from 'vitest'
+import {
+  SendNotificationUseCase,
+  type SendNotificationUseCaseRequest,
+  type SendNotificationUseCaseResponse,
+} from '../use-cases/send-notification'
+import { OnQuestionBestAnswerChosen } from './on-question-best-answer-chosen'
+
+let inMemoryQuestionsAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryAnswerRepository: InMemoryAnswerRepository
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sendNotificationUseCase: SendNotificationUseCase
+
+let sendNotificationExecuteSpy: MockInstance<
+  [SendNotificationUseCaseRequest],
+  Promise<SendNotificationUseCaseResponse>
+>
+
+describe('On Question Best Answer Chosen', () => {
+  beforeEach(() => {
+    inMemoryQuestionsAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionsAttachmentsRepository,
+    )
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswerRepository = new InMemoryAnswerRepository(
+      inMemoryAnswerAttachmentsRepository,
+    )
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sendNotificationUseCase = new SendNotificationUseCase(
+      inMemoryNotificationsRepository,
+    )
+
+    sendNotificationExecuteSpy = vi.spyOn(sendNotificationUseCase, 'excute')
+
+    new OnQuestionBestAnswerChosen(
+      inMemoryAnswerRepository,
+      sendNotificationUseCase,
+    )
+  })
+
+  it('should send a notification when question has neew best answer chosen', async () => {
+    const question = makeQuestion()
+    const answer = makeAnswer({ questionId: question.id })
+
+    inMemoryQuestionsRepository.create(question)
+    inMemoryAnswerRepository.create(answer)
+
+    question.bestAnswerId = answer.id
+    inMemoryQuestionsRepository.save(question)
+
+    await waitFor(() => {
+      expect(sendNotificationExecuteSpy).toHaveBeenCalled()
+    })
+  })
+})
