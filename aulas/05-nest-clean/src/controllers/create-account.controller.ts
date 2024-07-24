e45b6cb04cd6ff54a0db1491be6e2758fd4e9382
pdro-lucas <pdrolucas.dev@gmail.com>
2024-07-23 22:14:17 -0300

    feat: initial project config

---
+++ aulas/05-nest-clean/src/controllers/create-account.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  ConflictException,
+  UsePipes,
+} from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with this email already exists')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
