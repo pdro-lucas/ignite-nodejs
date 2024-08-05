db0ca213d6ed144d1d0a48ff88d66826ec1d81cc
pdro-lucas <pdrolucas.dev@gmail.com>
2024-08-05 20:22:50 -0300

    feat: create mappers

---
+++ aulas/05-nest-clean/src/infra/database/mappers/prisma-question-attachment-mapper.ts
@@ -0,0 +1,19 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { Attachment as PrismaAttachment } from '@prisma/client'
+
+export class PrismaQuestionAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): QuestionAttachment {
+    if (!raw.questionId) {
+      throw new Error('Question Attachment must have an questionId')
+    }
+
+    return QuestionAttachment.create(
+      {
+        questionId: new UniqueEntityID(raw.questionId),
+        attachmentId: new UniqueEntityID(raw.id),
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
